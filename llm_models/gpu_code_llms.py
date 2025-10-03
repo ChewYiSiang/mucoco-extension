8b24765913261af226b1768574145fb5191a17f4
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-10-03 12:27:05 +0100

    update model loading to allow remote files for TransformersCodeLLM

--- llm_models/gpu_code_llms.py
+++ llm_models/gpu_code_llms.py
@@ -8,7 +8,7 @@ class TransformersCodeLLM(CodeLLM):
     def __init__(self, model_name: str, answers: List[Any] = None) -> None:
         super().__init__(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, local_files_only = True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, local_files_only = False)
         if torch.cuda.is_available():
             self.model = self.model.to("cuda")
         
